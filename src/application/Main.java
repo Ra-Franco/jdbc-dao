3ca44432b5a041610eb815079308892f56276c39
RamonFranco <ramonccastro@hotmail.com>
2023-03-14 23:41:24 -0300

    Criação da classe Departement

---
+++ src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Departement obj = new Departement(1,"Books");
+		
+		System.out.println(obj);
+		
+	}
+
+}
