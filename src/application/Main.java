cc8d7f7bb93da2f3665c6f3ed4438e2912081225
RamonFranco <ramonccastro@hotmail.com>
2023-03-14 23:48:36 -0300

    Criação da classe Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departement;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Departement obj = new Departement(1,"Books");
+		Seller objS = new Seller(1, "Marco", "Marco@gmail.com", new Date(), 1200.00, obj);
 		
 		System.out.println(obj);
+		System.out.println(objS);
 		
 	}
 
