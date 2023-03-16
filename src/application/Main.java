3447361a9125c1d19817753b992364320da977c2
RamonFranco <ramonccastro@hotmail.com>
2023-03-15 22:54:31 -0300

    Criação findById da classe SellerDaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1,"Books");
-		Seller objS = new Seller(1, "Marco", "Marco@gmail.com", new Date(), 1200.00, obj);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Seller seller = sellerDao.findById(3);
 		
-		System.out.println(obj);
-		System.out.println(objS);
+		System.out.println(seller);
 		
 	}
 
